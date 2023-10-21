e9a43562447c9befc8a6780c57c8476d3d6c3ff0
Zou Jiale <2264284870@qq.com>
2023-10-21 12:15:19 +0800

    Update old repo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ProgramSpaceTeam.github.io
-This is studio's website. Welcome to join us.
+# programspacestudio.github.io
+Studio website.
+### 
+## 开源协议
+MIT开源协议，使用和分发时请遵守MIT开源协议！我们会给予开发者们最大限度的权限和自由！
---
+++ script.js
@@ -0,0 +1,20 @@
+function yejian() {
+    document.body.style.transition = 'all 0.5s'
+    document.body.style.color = '#ddd'
+    document.body.style.backgroundColor = '#101010'
+}
+
+function baitian() {
+    document.body.style.transition = 'all 0.5s'
+    document.body.style.color = '#000'
+    document.body.style.backgroundColor = 'rgba(255, 255, 255, 0.8)'
+}
+
+function editsvg() {
+    var svgc = document.getElementById('yb')
+    if (yj) {
+        svgc.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></svg>'
+    } else {
+        svgc.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></svg>'
+    }
+}
